1518913e3a8cd274796efec7bdaf147959095347
HenriqueSD <hsd.foz@gmail.com>
2019-07-23 21:03:36 -0300

    Implemented open gif with change page

--- gifs_searcher/lib/ui/home.page.dart
+++ gifs_searcher/lib/ui/home.page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 
+import 'gif_page.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -17,13 +19,13 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if (_search == null)
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=xxxxxxxx&limit=20&rating=G");
-    else
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=xxxxxxxx&q=$_search&limit=$_offset&offset=75&rating=G&lang=en");
+  if (_search == null)
+    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=xxxxxxxx&limit=20&rating=G");
+  else
+    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=xxxxxxxx&q=$_search&limit=$_offset&offset=75&rating=G&lang=en");
 
-      return json.decode(response.body);
-    }
+    return json.decode(response.body);
+  }
   
   @override
   void initState() {
@@ -114,6 +116,11 @@ class _HomePageState extends State<HomePage> {
             child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
               height: 300.0,
               fit: BoxFit.cover,),
+              onTap: (){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
+                );
+              },
             );
           else
             return Container(
